c621c79cfcd75daf4f99c929df89ea0b7a083558
sachin124 <sachinb2193@gmail.com>
2022-08-27 23:19:42 +0530

    widget

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Accordian from './component/Accordian';
+import Dropdown from './component/Dropdown';
+import Header from './component/Header';
+import Route from './component/Routs';
+import Search from './component/Search';
+import Translate from './component/Translate';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const items = [
+    {
+        title: 'What is React',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why user React',
+        content: 'React is a famous javascript library amoung engineers'
+    },
+    {
+        title: 'How does you use react',
+        content: 'You can use React by creating components'
+    }
+];
 
-export default App;
+const options = [
+    {
+        label: "The color of Red",
+        value: 'red'
+    },
+    {
+        label: 'The color of Green',
+        value: 'green'
+    },
+    {
+        label: 'A shade of Blue',
+        value: 'blue'
+    }
+];
+
+
+export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
+    return (
+        <div>
+            <Header />
+            <Route path='/'>
+                <Accordian items={items} />
+            </Route>
+
+
+            <Route path='/list'>
+                <Search />
+            </Route>
+
+            <Route path='/dropdown'>
+                <Dropdown
+                    label="Select a color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+
+            <Route path='/translate'>
+                <Translate />
+            </Route>
+        </div>
+    );
+};
